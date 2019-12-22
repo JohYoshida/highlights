5237967ef9886a672adcae5945f3bc68119ff419
Joh Yoshida <johyoshida@gmail.com>
2019-12-22 15:46:16 -0800

    Basic form and functionality

--- screens/AddProductScreen.js
+++ screens/AddProductScreen.js
@@ -1,33 +1,102 @@
-import React from 'react';
-import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import React from "react";
+import {
+  Button,
+  Picker,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View
+} from "react-native";
 import ActionButton from "react-native-action-button";
-import Icon from 'react-native-vector-icons/Ionicons';
+import Icon from "react-native-vector-icons/Ionicons";
+
+const { URL } = require("../constants/EnvironmentVariables");
 
 export default class AddProductScreen extends React.Component {
   static navigationOptions = {
-    title: 'Add Strain',
+    title: "Add Product",
     tabBarVisible: false
   };
 
   constructor(props) {
     super(props);
     this.state = {
+      producer_id: "",
+      strain_id: ""
     };
   }
 
   render() {
+    const { producers, strains } = this.props.navigation.state.params;
+    const Producers = [];
+    producers.forEach(item => {
+      Producers.push(
+        <Picker.Item label={item.name} value={item.id} key={item.id} />
+      );
+    });
+    const Strains = [];
+    strains.forEach(item => {
+      Strains.push(
+        <Picker.Item
+          label={item.name + " - " + item.type}
+          value={item.id}
+          key={item.id}
+        />
+      );
+    });
+
     return (
       <View style={styles.container}>
-        <Text>Add Strain</Text>
+        <Text>Producer</Text>
+        <Picker
+          selectedValue={this.state.producer_id}
+          style={{ height: 50, width: 100 }}
+          onValueChange={(itemValue, itemIndex) =>
+            this.setState({ producer_id: itemValue })
+          }
+        >
+          {Producers}
+        </Picker>
+        <Text>Strain</Text>
+        <Picker
+          selectedValue={this.state.strain_id}
+          style={{ height: 50, width: 100 }}
+          onValueChange={(itemValue, itemIndex) =>
+            this.setState({ strain_id: itemValue })
+          }
+        >
+          {Strains}
+        </Picker>
+        <Button title="Add Product" onPress={this.post.bind(this)} />
       </View>
     );
+  }
+
+  post() {
+    const { producer_id, strain_id } = this.state;
+    console.log(producer_id, strain_id);
+    fetch(`${URL}/products/${producer_id}/${strain_id}`, {
+      method: "POST"
+    })
+      .then(res => res.json())
+      .then(json => {
+        console.log(json);
+        this.props.navigation.goBack();
+      })
+      .catch(err => {
+        console.log("Error:\n" + err);
+        console.log(err);
+        this.props.navigation.goBack();
+      });
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
     paddingTop: 15,
-    backgroundColor: '#fff',
-  },
+    backgroundColor: "#fff"
+  }
 });
