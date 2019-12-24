46ef192a5d733922c82ed00b7712a60c267a6f3f
Joh Yoshida <johyoshida@gmail.com>
2019-12-23 21:29:30 -0800

    Add action button that navigates to AddProducerScreen and refreshes data

--- screens/ListProducersScreen.js
+++ screens/ListProducersScreen.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { FlatList, ScrollView, StyleSheet, Text, View } from "react-native";
 import { ListItem } from "react-native-elements";
+import ActionButton from "react-native-action-button";
+import Icon from "react-native-vector-icons/Ionicons";
 
 export default class ListProducersScreen extends React.Component {
   static navigationOptions = {
@@ -17,15 +19,25 @@ export default class ListProducersScreen extends React.Component {
 
   render() {
     return (
-      <ScrollView style={styles.container}>
-        <FlatList
-          data={this.state.producers}
-          renderItem={this.renderItem}
-          keyExtractor={(item, index) => index.toString()}
-          onRefresh={this.refresh}
-          refreshing={this.state.refreshing}
-        />
-      </ScrollView>
+      <View style={styles.container}>
+        <ScrollView>
+          <FlatList
+            data={this.state.producers}
+            renderItem={this.renderItem}
+            keyExtractor={(item, index) => index.toString()}
+            onRefresh={this.refresh}
+            refreshing={this.state.refreshing}
+            />
+        </ScrollView>
+        <ActionButton
+          buttonColor="#e74c3c"
+          onPress={() => this.props.navigation.navigate("AddProducer", {
+            refreshData: this.refresh
+          })}
+          >
+          <Icon name="md-add" />
+        </ActionButton>
+      </View>
     );
   }
 
